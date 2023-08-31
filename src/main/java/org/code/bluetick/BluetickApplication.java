9d46ceb04bbe901485982fd2a3aee8c7527143d6
Arpit Gupta <arpgupta@redhat.com>
2023-09-01 00:35:56 +0530

    Initial commit

--- src/main/java/org/code/bluetick/BluetickApplication.java
+++ src/main/java/org/code/bluetick/BluetickApplication.java
@@ -2,7 +2,6 @@ package org.code.bluetick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BluetickApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv(("MAIL_USERNAME")));
+		System.out.println(System.getenv(("MAIL_PASSWORD")));
+		System.out.println(System.getenv(("DB_USERNAME")));
+		System.out.println(System.getenv(("DB_PASSWORD")));
 		SpringApplication.run(BluetickApplication.class, args);
 	}
 
